533be81d5146dfb0a1c35ff4bdab3f2569d472fe
peter <peterwangjiawei@gmail.com>
2020-10-13 23:33:57 -0500

    Oct 9 React Native 123

--- App.js
+++ App.js
@@ -4,12 +4,13 @@ import {Demo} from './src/FlexView/demo1'
 import {Demo2} from './src/FlexView/demo2'
 import {Demo3} from './src/FlexView/demo3'
 import {Demo4} from './src/FlexView/demo4'
+import {ButtonStyle} from "./src/基本组件/ButtonStyle";
 
 export default function App() {
   return (
     <View style={styles.container}>
-
-    // </View>
+        <ButtonStyle/>
+    </View>
   );
 }
 
---
+++ src/基本组件/ButtonStyle.js
@@ -0,0 +1,9 @@
+import {StyleSheet, View, Button, Text} from 'react-native'
+
+export function ButtonStyle() {
+    return (
+        <View>
+            <Button onpress={}>Click</Button>
+        </View>
+    )
+}
